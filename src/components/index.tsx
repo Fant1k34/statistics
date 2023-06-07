29192c2e6a4a37e26af7c622198179d9d94bce59
Nikita Dukin <NDukin@alfabank.ru>
2023-06-08 01:26:35 +0300

    fix(*): fix not existing page

--- src/components/index.tsx
+++ src/components/index.tsx
@@ -7,7 +7,13 @@ import '@fontsource/roboto/500.css';
 import '@fontsource/roboto/700.css';
 import { Provider } from 'react-redux';
 import { store } from '../store';
+import {
+    createBrowserRouter,
+    RouterProvider,
+} from "react-router-dom";
 import styles from './index.module.css';
+import { Statistics } from '../statistics/statistics'
+import { contextRoot } from "../config";
 
 const QuizApplication = () => (
     <Provider store={store}>
@@ -17,12 +23,26 @@ const QuizApplication = () => (
     </Provider>
 );
 
+const router = createBrowserRouter([
+    {
+        path: `${contextRoot}/`,
+        element: <QuizApplication />,
+    },
+    {
+        path: `${contextRoot}Quiz/`,
+        element: <QuizApplication />,
+    },
+    {
+        path: `${contextRoot}statistics/`,
+        element: <Statistics />,
+    },
+]);
 
 const container = document.getElementById("app");
 const root = createRoot(container as HTMLElement);
 
 root.render(
     <div className={styles.outerContainer}>
-        <QuizApplication />
+        <RouterProvider router={router} />
     </div>
 );
